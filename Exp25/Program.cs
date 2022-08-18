888ccc52f4c65450d03257c55f5ce29f3de37e1c
evgenii <evgenii_08@list.ru>
2022-08-19 01:05:44 +0300

    init4

--- Exp25/Program.cs
+++ Exp25/Program.cs
@@ -2,16 +2,19 @@
 // натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-
+void Step(int A, int B)
+{
+  int num = 1;
+  int i = 1;
+  while (i <= B)
+    {
+      num = num * A;
+      i++;
+    }
+   Console.Write(num);
+}
 Console.Write("Введите число А: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
-int num = 1;
-int i = 1;
-while (i <= B)
-{
-    num = num * A;
-    i++;
-}
-Console.Write(A);
+Step(A,B);
